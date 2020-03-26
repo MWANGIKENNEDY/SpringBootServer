213d630bb4c66bd6886ec27aee43dc50f7191acb
MWANGIKENNEDY <kenmwan75@gmail.com>
2020-03-27 02:45:36 +0300

    Added optional method for fetching one record

--- src/main/java/com/kennedy/backend/controller/UserController.java
+++ src/main/java/com/kennedy/backend/controller/UserController.java
@@ -4,9 +4,13 @@ import com.kennedy.backend.model.UserModel;
 import com.kennedy.backend.service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,13 +19,14 @@ public class UserController {
     @Autowired
     UserService userService;
     
-    @GetMapping(value="/users")
-    public List<UserModel> users(){
-        return userService.getAllUsers();
+    @RequestMapping(value="/users",method=RequestMethod.GET)
+    public ResponseEntity<Object> users(){
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
     }
     
-    @PostMapping(value="/users/register")
-    public void registerUser(@RequestBody UserModel userModel){
-        userService.register(userModel);    
+    @RequestMapping(value="/users/register",method=RequestMethod.POST)
+    public ResponseEntity<Object> registerUser(@RequestBody UserModel userModel){
+        userService.register(userModel); 
+        return new ResponseEntity<>("User registered",HttpStatus.CREATED);
     }
 }
