7b2f968b432bd094a8b4dfcdf485533ce6412337
MWANGIKENNEDY <kenmwan75@gmail.com>
2020-03-27 03:02:06 +0300

    Implemented the delete and update functionalities

--- src/main/java/com/kennedy/backend/controller/UserController.java
+++ src/main/java/com/kennedy/backend/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +19,31 @@ public class UserController {
     
     @Autowired
     UserService userService;
-    
-    @RequestMapping(value="/users",method=RequestMethod.GET)
+    //GET ALL USERS
+    @RequestMapping(value="/api/users",method=RequestMethod.GET)
     public ResponseEntity<Object> users(){
         return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
     }
-    
-    @RequestMapping(value="/users/register",method=RequestMethod.POST)
+    //CREATE A NEW USER
+    @RequestMapping(value="/api/users/",method=RequestMethod.POST)
     public ResponseEntity<Object> registerUser(@RequestBody UserModel userModel){
         userService.register(userModel); 
         return new ResponseEntity<>("User registered",HttpStatus.CREATED);
     }
+    //UPDATE USER DETAILS
+    @RequestMapping(value="/api/users/{id}",method=RequestMethod.PUT)
+    public ResponseEntity<Object> updateUser(@PathVariable("id") Long id, @RequestBody UserModel userModel){
+        userModel.setId(id);
+        userService.updateUser(userModel);
+        return new ResponseEntity<>("User updated !",HttpStatus.OK);
+    }
+    //delete a user from the system
+    @RequestMapping(value="/api/users/{id}",method=RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteUser(@PathVariable("id") Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>("User deleted !",HttpStatus.OK);
+    }
+    
+    
+    
 }
--- src/main/java/com/kennedy/backend/service/UserService.java
+++ src/main/java/com/kennedy/backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.kennedy.backend.service;
 import com.kennedy.backend.model.UserModel;
 import com.kennedy.backend.repository.UserRepo;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,19 @@ public class UserService {
     }
     //create a new user
     public void register(UserModel userModel){
+        userRepo.save(userModel);   
+    }
+    //get one user/post
+    public Optional<UserModel> getOneUser(Long id){
+        return userRepo.findById(id);
+    }
+    //update user details
+    public void updateUser(UserModel userModel){
         userRepo.save(userModel);
-        
+    }
+    //delete user
+    public void deleteUser(Long id){
+        userRepo.deleteById(id);
     }
     
 }
